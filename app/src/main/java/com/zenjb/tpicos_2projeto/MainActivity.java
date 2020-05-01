1c0740934a5993fc263e84458f7b331f3079e371
ZenJB <0zenjb0@gmail.com>
2020-05-01 13:33:10 +0100

    Primeiro Release

---
+++ app/src/main/java/com/zenjb/tpicos_2projeto/MainActivity.java
@@ -0,0 +1,23 @@
+package com.zenjb.tpicos_2projeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+
+    }
+}
