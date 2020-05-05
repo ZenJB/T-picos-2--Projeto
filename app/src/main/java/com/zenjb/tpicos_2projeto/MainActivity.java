fd5b9e3b774ae6e348f39954f17b99cbfd813267
André Ornelas Telo <2042316@student.uma.pt>
2020-05-05 17:39:54 +0100

    some changes

--- app/src/main/java/com/zenjb/tpicos_2projeto/MainActivity.java
+++ app/src/main/java/com/zenjb/tpicos_2projeto/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         final WebView webView = (WebView) findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         //webView.loadUrl("file:///android_asset/index.html");
