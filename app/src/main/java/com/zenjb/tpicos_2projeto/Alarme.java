428842679e71825d3d7f035978dfcbe56b298938
ZenJB <0zenjb0@gmail.com>
2020-05-08 18:35:43 +0100

    Simplificado o código

---
+++ app/src/main/java/com/zenjb/tpicos_2projeto/Alarme.java
@@ -0,0 +1,6 @@
+package com.zenjb.tpicos_2projeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Alarme extends AppCompatActivity {
+}
--- app/src/main/java/com/zenjb/tpicos_2projeto/MainActivity.java
+++ app/src/main/java/com/zenjb/tpicos_2projeto/MainActivity.java
@@ -1,10 +1,18 @@
 package com.zenjb.tpicos_2projeto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +26,54 @@ public class MainActivity extends AppCompatActivity {
         //webView.loadUrl("file:///android_asset/definicoes.html");
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        webView.addJavascriptInterface(this, "Android");
 
+    }
+
+
+    @JavascriptInterface
+    public void showNotification(String title, String message){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "lemubitA")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(100, builder.build());
+    }
+
+    /** Show a toast from the web page */
+    @JavascriptInterface
+    public void showToast(String toast) {
+        Toast.makeText(this, toast, Toast.LENGTH_SHORT).show();
+    }
+
+    @JavascriptInterface
+    public void showAlert(String title, String message){
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    @JavascriptInterface
+    public String getSetting(String setting){
+        SharedPreferences sharedPreferences = this.getSharedPreferences(
+                "com.topicos.despertador", Context.MODE_PRIVATE);
+        return sharedPreferences.getString(setting, "null");
+    }
+
+    @JavascriptInterface
+    public boolean saveSetting(String setting, String data){
+        SharedPreferences sharedPreferences = this.getSharedPreferences(
+                "com.topicos.despertador", Context.MODE_PRIVATE);
+        return sharedPreferences.edit().putString(setting, data).commit();
     }
 }
