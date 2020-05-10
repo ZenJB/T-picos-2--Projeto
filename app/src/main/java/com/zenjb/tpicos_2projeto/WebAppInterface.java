1579fd8bbdcbd62f0632e73a993a0ff58aa96747
André Ornelas Telo <2042316@student.uma.pt>
2020-05-10 09:18:42 +0100

    notifications working in real device

--- app/src/main/java/com/zenjb/tpicos_2projeto/WebAppInterface.java
+++ app/src/main/java/com/zenjb/tpicos_2projeto/WebAppInterface.java
@@ -2,10 +2,14 @@ package com.zenjb.tpicos_2projeto;
 
 
 import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Build;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
 
@@ -21,6 +25,8 @@ public class WebAppInterface {
         showNotification("Despertador", "Sabia que se deitar tarde faz mal ao seu cerebro?");
     }
 
+
+
     public void showNotification(String title, String message){
         NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext, "lemubitA")
                 .setSmallIcon(R.drawable.ic_launcher_background)
@@ -30,7 +36,24 @@ public class WebAppInterface {
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(mContext);
         notificationManagerCompat.notify(100, builder.build());
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "definicao do alarme";
+            String description = "o alarme foi definido";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("lemubitA", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = mContext.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
     }
+
+
 
     /** Show a toast from the web page */
     @JavascriptInterface
